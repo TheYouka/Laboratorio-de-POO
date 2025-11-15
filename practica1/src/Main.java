23ba506ab33d832d89b4145ffd2564ce1c8381ec
TheYouka <168247469+TheYouka@users.noreply.github.com>
2025-11-15 13:21:39 -0600

    Add files via upload

---
+++ practica1/src/Main.java
@@ -0,0 +1,15 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+void main() {
+    System.out.println("Hello, World!");
+
+    System.out.println("DEpurando conflito");
+
+    Scanner scanner = new Scanner(System.in);
+
+    String mensaje = scanner.nextLine();
+    System.out.println(mensaje);
+
+    int x = 2+2;
+    System.out.println(x);
+}
